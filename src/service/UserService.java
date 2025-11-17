e3933036048c3a6ff82abdd9b1ba42f17db48e88
eyadmotawe <eyadmotawe@gmail.com>
2025-11-17 03:13:07 +0200

    UserService.java

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
-/**
- *
- * @author DELL
- */
+
+import test.database.JsonDatabaseManager;
+import test.model.User;
+
 public class UserService {
+    private final JsonDatabaseManager dbManager;
     
+    public UserService(JsonDatabaseManager dbManager) {
+        this.dbManager = dbManager;
+    }
+    
+  
+    public User getUserById(String userId) {
+        return dbManager.getUserById(userId);
+    }
+    
+
+    public User getUserByEmail(String email) {
+        return dbManager.getUserByEmail(email);
+    }
+    
+
+    public void updateUser(User user) {
+        dbManager.updateUser(user);
+    }
 }
